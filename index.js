5bd9f874938b68bd47d30cfeb6512a4b5b554e6b
Andrew <7147407+ha-noh@users.noreply.github.com>
2020-10-24 22:46:31 -0400

    Changed backspacing implementation
    + Backspacing is now detected based on the actual keypress. The previous implementation simply inferred whether this was the case based on whether the input value was empty.

--- index.js
+++ index.js
@@ -7,25 +7,35 @@ function handleInput(e) {
   if (input.nextElementSibling && input.value) {
     input.nextElementSibling.focus();
   }
-  // if the user cleared the input field, focus the preceding input (if it exists)
-  // *might be worth considering to achieve this with a keyup event listener
-  else if (input.previousElementSibling && !input.value) {
-  	input.previousElementSibling.focus();
-  }
 }
 
 function handlePaste(e) {
   const paste = e.clipboardData.getData('text');
   // loop over each input, and populate with the index of that string
   inputs.forEach((input, i) => {
-    console.log(input);
     input.value = paste[i] || '';
   });
+}
+
+function handleKeyup(e) {
+	const key = e.keyCode;
+	const target = e.target;
+	//if backspace was pressed on an input element, focus its preceding sibling (if it exists)
+	if (key == 8 && target.tagName === 'INPUT' && target.previousElementSibling) {
+		//delete the target input's value first before focusing because of when the keydown event triggers
+		// target.value = '';
+		target.previousElementSibling.focus();
+	}
 }
 
 inputs[0].addEventListener('paste', handlePaste);
 
 form.addEventListener('input', handleInput);
+
+/* using the keydown event would allow the user to hold backspace to clear multiple inputs, 
+ * but fixing that behavior for a single tap is complicated
+ */
+form.addEventListener('keyup', handleKeyup);
 
 // 1. select the text when the next input is focued
 // 2. Auto submit the form if all fields are filled after a paste
